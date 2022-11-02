d0af2a3f53a20226483b40132ff7fe4771110bdf
theolorenzi <theo.lorenzi@laposte.net>
2022-11-02 11:16:42 +0100

    Premier commit : début du projet

---
+++ src/main/java/com/inti/Td1SpringCorrApplication.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td1SpringCorrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Td1SpringCorrApplication.class, args);
+	}
+
+}
